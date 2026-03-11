0569416a9f575d618874fc2d388e06816e03fc5a
luanssouza7-netizen <luan.s.souza7@ba.estudante.senai.br>
2026-03-11 13:38:21 -0300

    Add files via upload

---
+++ questao2.py
@@ -0,0 +1,12 @@
+import os
+os.system("cls  ||  clear")
+
+nome =str(input("Digite seu nome : "))
+sexo =str(input("Digite seu sexo, masculino ou feminino : "))
+estadoCivil =str(input("Casado ou solteiro : "))
+feminino = str(input)
+maculino = str(input)
+casado = str(input)
+casada =str(input)
+anosDeCasamento = int(input("Digite quantos anos de casada :" ))
+print(f"Quanstos anos de casada ? {anosDeCasamento}")
