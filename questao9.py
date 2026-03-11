68fcf057692d6f60ab12bc346052425b192570df
luanssouza7-netizen <luan.s.souza7@ba.estudante.senai.br>
2026-03-11 14:48:36 -0300

    Add files via upload

---
+++ questao9.py
@@ -0,0 +1,19 @@
+import os 
+os. sytem("cls  ||  clear")
+
+rendamental = int(input("Quanto voce recebe no mês ? : "))
+
+emprestimo = 10 = rendamental
+
+print(f"O emprestimo ficou de : {emprestimo}")
+
+prestação = rendamental *30/100
+print(f"O emprestimo ficou de : {prestação}")
+
+parcelas = emprestimo / prestação
+print(f"você derevá pegar :{parcelas:.1f} por mas : ")
+
+print("Ira querer o emprestimo ?:")
+efetivação = str(input("sim ou não : "))
+ 
+print("obrigatorio")
